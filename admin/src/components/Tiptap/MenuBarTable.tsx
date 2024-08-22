abca61276245a448db42f4670b64875a3a9e5ea9
Marcin Stawowczyk <merlin@magictm.com>
2024-08-22 22:57:49 +0200

    Update MenuBarTable.tsx

--- admin/src/components/Tiptap/MenuBarTable.tsx
+++ admin/src/components/Tiptap/MenuBarTable.tsx
@@ -11,26 +11,52 @@ import {
     AiOutlineMergeCells,
     AiOutlineSplitCells,
 } from 'react-icons/ai'
+import { PiSquareHalf, PiSquareHalfBottom,PiRectangle  } from 'react-icons/pi'
 import { Fragment } from 'react/jsx-runtime'
-
 const MenuBarTable = ({ editor }) => {
     return (
         <Fragment key="tableMenubar">
             <IconButtonGroup className="button-group">
                 <IconButton
+                    label="Toggle Header Column"
+                    className={['large-icon']}
+                    icon={<PiSquareHalf />}
+                    onClick={() => editor.chain().focus().toggleHeaderColumn().run()}
+                    disabled={!editor.can().toggleHeaderColumn()}
+                />
+                <IconButton
+                    label="Toggle Header Row"
+                    className={['large-icon']}
+                    icon={<PiSquareHalfBottom />}
+                    onClick={() => editor.chain().focus().toggleHeaderRow().run()}
+                    disabled={!editor.can().toggleHeaderRow()}
+                />
+                <IconButton
+                    label="Toggle Header Cell"
+                    className={['large-icon']}
+                    icon={<PiRectangle />}
+                    onClick={() => editor.chain().focus().toggleHeaderCell().run()}
+                    disabled={!editor.can().toggleHeaderCell()}
+                />
+            </IconButtonGroup>
+            <IconButtonGroup className="button-group">
+                <IconButton
                     icon={<AiOutlineInsertRowLeft />}
+                    className={['large-icon']}
                     label="Add column before"
                     onClick={() => editor.chain().focus().addColumnBefore().run()}
                     disabled={!editor.can().addColumnBefore()}
                 />
                 <IconButton
                     icon={<AiOutlineInsertRowRight />}
+                    className={['large-icon']}
                     label="Add column after"
                     onClick={() => editor.chain().focus().addColumnAfter().run()}
                     disabled={!editor.can().addColumnAfter()}
                 />
                 <IconButton
                     icon={<AiOutlineDeleteColumn />}
+                    className={['large-icon']}
                     label="Delete column"
                     onClick={() => editor.chain().focus().deleteColumn().run()}
                     disabled={!editor.can().deleteColumn()}
@@ -39,18 +65,21 @@ const MenuBarTable = ({ editor }) => {
             <IconButtonGroup className="button-group">
                 <IconButton
                     icon={<AiOutlineInsertRowAbove />}
+                    className={['large-icon']}
                     label="Add row before"
                     onClick={() => editor.chain().focus().addRowBefore().run()}
                     disabled={!editor.can().addRowBefore()}
                 />
                 <IconButton
                     icon={<AiOutlineInsertRowBelow />}
+                    className={['large-icon']}
                     label="Add row after"
                     onClick={() => editor.chain().focus().addRowAfter().run()}
                     disabled={!editor.can().addRowAfter()}
                 />
                 <IconButton
                     icon={<AiOutlineDeleteRow />}
+                    className={['large-icon']}
                     label="Delete row"
                     onClick={() => editor.chain().focus().deleteRow().run()}
                     disabled={!editor.can().deleteRow()}
@@ -60,11 +89,13 @@ const MenuBarTable = ({ editor }) => {
             <IconButtonGroup className="button-group">
                 <IconButton
                     icon={<AiOutlineMergeCells />}
+                    className={['large-icon']}
                     label="Merge cells"
                     onClick={() => editor.chain().focus().mergeCells().run()}
                 />
                 <IconButton
                     icon={<AiOutlineSplitCells />}
+                    className={['large-icon']}
                     label="Split cells"
                     onClick={() => editor.chain().focus().splitCell().run()}
                 />
@@ -73,6 +104,7 @@ const MenuBarTable = ({ editor }) => {
             <IconButtonGroup className="button-group">
                 <IconButton
                     icon={<AiOutlineDelete />}
+                    className={['large-icon']}
                     label="Delete table"
                     disabled={!editor.can().deleteTable()}
                     onClick={() => {
