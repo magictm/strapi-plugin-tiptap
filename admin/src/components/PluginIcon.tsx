09eb40f0f31dbe0abdfcd6a9217d8503eba99994
Marcin Stawowczyk <merlin@magictm.com>
2024-08-27 17:25:04 +0200

    Update PluginIcon.tsx

--- admin/src/components/PluginIcon.tsx
+++ admin/src/components/PluginIcon.tsx
@@ -1,7 +1,5 @@
-import { Flex, Icon } from '@strapi/design-system'
-import { Paint } from '@strapi/icons'
+import { Flex } from '@strapi/design-system'
 import styled from 'styled-components'
-import React from 'react'
 
 const IconBox = styled(Flex)`
     /* Hard code color values */
@@ -24,7 +22,29 @@ export const PluginIcon = () => {
             hasRadius
             aria-hidden
         >
-            <Icon as={Paint} />
+            <MyIcon />
         </IconBox>
     )
 }
+
+const MyIcon = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        id="recktangle_logo"
+        data-name="recktangle logo"
+        viewBox="0 0 675.91 495.67"
+        {...props}
+    >
+        <defs>
+            <style>{'.cls-1{fill:#010101;stroke-width:0}'}</style>
+        </defs>
+        <path
+            d="M189.42 178.76h38.9v-38.9h63.25l-50.02 233.38h-52.12v38.9h155.59v-38.9h-43.8l50.02-233.38h71.57v38.9h38.9v-77.79H189.42v77.79Z"
+            className="cls-1"
+        />
+        <path
+            d="M462.3 265.61v146.53h-19.54V265.61h-58.61v-19.54h136.76v19.54H462.3Z"
+            className="cls-1"
+        />
+    </svg>
+)
